a59518e599069f01c2d517f3a3e4e9b1b51f87e1
magbor21 <89779631+magbor21@users.noreply.github.com>
2021-10-13 12:50:26 +0200

    Add project files.

---
+++ ToDoIt4/Model/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoIt4.Model
+{
+    class Class1
+    {
+    }
+}
---
+++ ToDoIt4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDoIt4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
